a8a13755150705446d4e7e310fa5533de31ec9aa
VIshnu Santhosh <60912431+Vishnuvichuz@users.noreply.github.com>
2020-02-11 15:50:34 +0530

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,2 +1,6 @@
 #This is my first project
-var person = [{Name:"Baluvo", class:"10", age:"22", display:function(){console.log(this.Name)}},{Name:"vichu", class:"10", age:"22", display:function(){console.log(this.Name)}}]
+var person = [{Name:"Baluvo", class:"10", age:"22", display:function(){console.log(this.Name)}},
+              {Name:"vichu", class:"10", age:"22", display:function(){console.log(this.Name)}},
+              {Name:"Arjun", class:"10", age:"22", display:function(){console.log(this.Name)}},
+              {Name:"Dileep",class:"22", display:function(){console.log(this.Name)}},
+              {Name:"vichu", class:"10", age:"22", display:function(){console.log(this.Name)}}]
