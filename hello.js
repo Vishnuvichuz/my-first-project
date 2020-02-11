00492b4fdc33f5df97226454faded0d8a791a2a0
VIshnu Santhosh <60912431+Vishnuvichuz@users.noreply.github.com>
2020-02-11 15:43:42 +0530

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,1 +1,2 @@
 #This is my first project
+var person = [{Name:"Baluvo", class:"10", age:"22", display:function(){console.log(this.Name)}},{Name:"vichu", class:"10", age:"22", display:function(){console.log(this.Name)}}]
